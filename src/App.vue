8d0a7fe02cca33956d38daab9e04459d78834c57
Arkesdev <mateusaqles@gmail.com>
2024-10-18 17:38:24 -0300

    fix bugs

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@ import Principal from './components/Principal.vue'
 </script>
 
 <template>
-  <section id="background" class="bg-cover bg-center px-[60px] py-[24px] mobile:px-[12px] mo:px-[190px] big:px-80">
+  <section id="background" class="bg-cover bg-center px-[60px] py-[24px] mobile:px-[12px] big:px-80">
     <Header/>
   </section>
-  <div class="px-[60px] mobile:px-[12px] mo:px-[190px] big:px-80">
+  <div class="px-[60px] mobile:px-[12px] big:px-80">
     <Principal/>
   </div>
 </template>
